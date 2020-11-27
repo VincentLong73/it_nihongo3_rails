0550e33692b6c29f25381906b5909bf31b8bf2f2
Ubuntu <ubuntu@ip-172-31-87-1.ec2.internal>
2020-11-27 14:09:08 +0000

    Add review book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,12 +10,14 @@ class BooksController < ApplicationController
   # GET /books/1
   # GET /books/1.json
   def show
+    @book_review = BookReview.new
   end
 
   # GET /books/new
   def new
     @book = Book.new
   end
+  
 
   # GET /books/1/edit
   def edit
